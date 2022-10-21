771a17a7bb1e9c520a045b40918a9acf1ec36463
ice breaker <1324318532@qq.com>
2022-10-21 10:11:01 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: false,
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: ['standard', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
